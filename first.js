57f33771fc8e4751c8b9faf327969105b82468a3
Himanshu Gupta <himanshugpt0005@gmail.com>
2025-01-26 13:56:17 +0530

    Bug Fixed by Me

--- first.js
+++ first.js
@@ -3,8 +3,15 @@ console.log(button);
 
 const login="Login page added";
 console.log(login);
-const Payment="Integrated the payment gateway";
-console.log(Payment);
+// const Payment="Integrated the payment gateway";
+// console.log(Payment);
 
 const upi="Integrated the UPI";
 console.log(upi);
+
+const footer="Footer added in out website"
+console.log(footer);
+console.log("latest update");
+
+//I am fixing sume Bug
+console.log("Bug Fixed");
